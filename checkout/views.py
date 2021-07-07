174440bbd8b6e1333a1b62db520dcace13df792d
Summersby95 <james.summersby@gmail.com>
2021-07-07 22:50:59 +0100

    add stripe payment functionality

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JAhPDGzNWLkO7k0ZCCtSnz9AZ6SklbskBAyJm6I91LOyRawQiAwaB1c4mjX4lHDguVESbK990tHfQAk6XWSHB8600Y9nVAKKL',
+        'client_secret': 'sk_test_jwzDYqzvH9KtbzrzfH0YKcw',
     }
 
     return render(request, template, context)
