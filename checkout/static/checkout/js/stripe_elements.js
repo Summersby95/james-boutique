6e1bea4985ac094369e23cacaab4f04a3b9095bb
Summersby95 <james.summersby@gmail.com>
2021-07-07 23:55:51 +0100

    add stripe payment functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,7 +1,9 @@
-var stripe_public_key = $("#id_stripe_public_key").text().slice(1, -1);
-var client_secret = $("#id_client_secret").text().slice(1, -1);
+// jshint: esversion: 6
 
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
+var clientSecret = $("#id_client_secret").text().slice(1, -1);
+
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 
 var style = {
@@ -21,6 +23,47 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 
-
-
 card.mount('#card-element');
+
+card.addEventListener('change', function(e) {
+    var errorDiv = document.getElementById('card-errors');
+    if (e.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${e.error.message}</span>`;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true });
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+      }
+    }).then(function(result) {
+      if (result.error) {
+        var errorDiv = document.getElementById('card-errors');
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${result.error.message}</span>`;
+        $(errorDiv).html(html);
+        card.update({ 'disabled': false });
+        $('#submit-button').attr('disabled', false);
+      } else {
+        if (result.paymentIntent.status === 'succeeded') {
+          form.submit();
+        }
+      }
+    });
+});
