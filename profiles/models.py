0cc4da62ba42a6286c74210427ac826fadc574fc
Summersby95 <james.summersby@gmail.com>
2021-07-13 21:44:39 +0100

    added profile update form, updated templates

--- profiles/models.py
+++ profiles/models.py
@@ -7,12 +7,12 @@ from django.db.models.signals import post_save
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=False)
-    default_country = CountryField(blank_label='Country *', null=True, blank=False)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=False)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=False)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=False)
     default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=False)
 
     def __str__(self):
         return self.user.username
